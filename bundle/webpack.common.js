4151f8a3d435b9504c1c1ae41e2c3d348c95b030
MatasMot <98148619+MatasMot@users.noreply.github.com>
2022-10-13 09:38:20 +0200

    The beginning

---
+++ bundle/webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "../build")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'template.html'
+        }),
+        new MiniCssExtractPlugin({
+            linkType: "text/css"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|sass|scss)$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|webp|gif|heic)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.js$/i,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import section from "./components/section.js"
+
+let element = document.querySelector(".wrapper")
+element.append(section())
